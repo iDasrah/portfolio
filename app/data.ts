6172f23a4a1eb41183738136a2eada66d59d1da2
Mathéo <matheo.picouleau@hotmail.com>
2025-04-28 16:45:07 +0200

    déplacement données (projets, skills) dans data.ts

---
+++ app/data.ts
@@ -0,0 +1,53 @@
+export const projects = [
+    {
+        title: "PuckTracker",
+        description: "PuckTracker est une application de suivi des performances NHL. Elle récupère, stocke et analyse les statistiques des joueurs (points, buts, passes) pour générer des comptes rendus par match et par équipe. L’outil met en avant les meilleurs pointeurs, buteurs et passeurs sur les derniers matchs et facilite l’analyse rapide des tendances.",
+        technologies: ["React", "TailwindCSS", "Prisma", "NestJS"],
+        link: "https://pucktracker.vercel.app/"
+    },
+    {
+        title: "Frise-moi ça!",
+        description: "Jeu de société en ligne inspiré de Timeline. Les joueurs doivent placer des événements historiques français sur une frise chronologique. Le but est de deviner la date d'un événement en le plaçant correctement sur la frise. Le jeu est accessible à tous et permet de tester ses connaissances historiques tout en s'amusant.",
+        technologies: ["React", "TailwindCSS", "Node.js", "Express", "Socket.io"],
+        link: "https://t4-frise-moi-ca.vercel.app/"
+    },
+    {
+        title: "Projet cours web : React",
+        description: "Application basée sur l'API créé en cours, permettant de gérer une base de données de livres et d'auteurs.",
+        technologies: ["React", "TailwindCSS"],
+        link: "https://github.com/iDasrah/projet-w42-react.git"
+    },
+    {
+        title: "Projet cours web : API",
+        description: "Création d'une API permettant de gérer une base de données de livres et d'auteurs.",
+        technologies: ["Node.js", "Express", "Prisma"],
+        link: "https://github.com/iDasrah/cours-nodejs-express-prisma.git"
+    },
+    {
+        title: "Projet cours Android",
+        description: "Application Android permettant de gérer une base de données de livres et d'auteurs.",
+        technologies: ["Java", "Android Studio"],
+        link: "https://github.com/iDasrah/android-books-client.git"
+    }
+];
+
+export const skills = [
+    "Next.js",
+    "React",
+    "React Native",
+    "Tailwind CSS",
+    "TypeScript",
+    "Node.js",
+    "Express",
+    "NestJS",
+    "Prisma",
+    "Framer Motion",
+    "API REST",
+    "PHP",
+    "Laravel",
+    "C",
+    "Java",
+    "C#",
+    "Python",
+    "SQL"
+];
