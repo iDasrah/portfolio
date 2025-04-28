e65969a78ad1fb74080c64e7b08b00e20316f007
Mathéo <matheo.picouleau@hotmail.com>
2025-04-28 16:45:54 +0200

    suppression styles inutiles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "@/app/globals.css";
 import { ReactNode } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="fr">
-            <body className="bg-black text-white font-sans flex flex-col min-h-screen">
+            <body className="bg-black text-white flex flex-col min-h-screen">
                 <Header />
-                <main className="flex-grow">{children}</main>
+                <main>{children}</main>
                 <Footer />
             </body>
         </html>
